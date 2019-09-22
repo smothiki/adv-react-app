55bb6986a5ea2c1dbc5d9e015641e0cc0a0cdbb8
sivaram mothiki <smothiki@sivarams-MacBook-Pro.local>
2019-09-21 18:23:07 -0700

    vidly movie app

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieManager from "./components/movieManager"
 
 class App extends Component {
+
+
   render() {
     return (
-      <main className="container">
-        <h1>hello world</h1>
-      </main>
+      <React.Fragment>
+        <MovieManager />
+      </React.Fragment >
     );
   }
 }
---
+++ vidly/src/components/movieManager.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { getMovies, deleteMovie } from '../services/fakeMovieService';
+
+
+class MovieManager extends Component {
+    state = {
+        movies: getMovies()
+    }
+
+    deletemoviecode(id) {
+        deleteMovie(id)
+        this.setState({ movies: getMovies() })
+    }
+
+    getmoviecode(movie) {
+        return <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td><button onClick={() => this.deletemoviecode({ id: movie._id })} className="btn btn-danger btn-sm">Delete</button></td>
+        </tr >
+    }
+
+    getallMovies() {
+        const allmovies = this.state.movies.map(movie => this.getmoviecode(movie))
+        console.log(allmovies)
+        return allmovies
+    }
+
+    render() {
+
+        if (this.state.movies.length === 0)
+            return <p ClassName="lead"> no movies in list</p>
+
+        return (
+
+            <table className="table">
+
+                <tr >
+                    <th scope="col">Title</th>
+                    <th scope="col">Genre</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col">Action</th>
+                </tr>
+                {this.getallMovies()}
+            </table>
+        );
+    }
+}
+
+export default MovieManager;
